a3314e3f1a40d1fe6e8c408a6c98a9cb68b8b4db
khasanjon <khasanjon.dev@gmail.com>
2024-04-21 22:50:01 +0500

    ready

--- backend/apps/news/serializers.py
+++ backend/apps/news/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, Serializer
 
 from .models import News
 
@@ -7,3 +7,7 @@ class NewsModelSerializer(ModelSerializer):
     class Meta:
         model = News
         fields = '__all__'
+
+
+class NoneSerializer(Serializer):
+    pass
--- backend/apps/news/views.py
+++ backend/apps/news/views.py
@@ -1,9 +1,21 @@
+from socket import gethostname
+
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from .models import News
-from .serializers import NewsModelSerializer
+from .serializers import NewsModelSerializer, NoneSerializer
+
+count = {gethostname(): 0}
 
 
 class NewsModelViewSet(ModelViewSet):
     queryset = News.objects.all()
     serializer_class = NewsModelSerializer
+
+    @action(['get'], False, 'balance', serializer_class=NoneSerializer)
+    def balance(self, request):
+        # result = {gethostname(): 0}
+        count[gethostname()] += 1
+        return Response({"message": f"{gethostname()} {count}"})
