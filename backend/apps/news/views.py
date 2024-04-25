142875d7b5671544ea087bc653bdb2a854957b3d
khasanjon <khasanjon.dev@gmail.com>
2024-04-25 22:44:11 +0500

    fix some actions

--- backend/apps/news/views.py
+++ backend/apps/news/views.py
@@ -1,4 +1,4 @@
-from _socket import gethostname
+from socket import gethostname
 
 from rest_framework.decorators import action
 from rest_framework.response import Response
@@ -7,14 +7,14 @@ from rest_framework.viewsets import ModelViewSet
 from .models import News
 from .serializers import NewsModelSerializer, NoneSerializer
 
-count = {gethostname(): 0}
+count = {}
 
 
 class NewsModelViewSet(ModelViewSet):
     queryset = News.objects.all()
     serializer_class = NewsModelSerializer
 
-    @action(['get'], False, 'balance', serializer_class=NoneSerializer)
-    def balance(self, request):
-        count[gethostname()] += 1
-        return Response({"message": f"{gethostname()} {count}"})
+    @action(detail=False, methods=['get'], url_path='counter', serializer_class=NoneSerializer)
+    def counter(self, request):
+
+        return Response()
